9539fe5db5b3eadd1ac81fe52adf78dda11216f2
Ashirvad Satapathy <74089931+dEvAshirvad@users.noreply.github.com>
2025-08-05 09:01:51 +0530

    feat: ch

--- src/configs/origins.ts
+++ src/configs/origins.ts
@@ -6,6 +6,13 @@ const origins = [
   'http://localhost:3030',
   'http://localhost:3031',
   'http://localhost:3032',
+  'https://kpiservice.rdmp.in',
+  'https://auth.rdmp.in',
+  'https://shresth.rdmp.in',
+  'https://rahat.rdmp.in',
+  'https://filesapi.rdmp.in',
+  'https://rahatapi.rdmp.in',
+  'https://urvi.rdmp.in',
 ];
 
 export default origins;
